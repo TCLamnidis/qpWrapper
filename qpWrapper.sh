cc8e91a0e66d5b8f94410de54586de0f6202c137
Thiseas Christos Lamnidis <lamnidis@mpi-cruncher1.sdag.ppj.shh.mpg.de>
2017-10-17 05:47:45 +0200

    Added "allsnps: YES" option. On by default. turned off with -a

--- qpWrapper.sh
+++ qpWrapper.sh
@@ -2,7 +2,7 @@
 
 #You should be editing ONLY the paths in lines 36-40! 
 
-TEMP=`getopt -q -o hS:R:L:D: --long help,Sample:,Right:,Ref:,Left:,Source:,SubDir: -n 'qpWrapper.sh' -- "$@"`
+TEMP=`getopt -q -o ha:S:R:L:D: --long help,Sample:,Right:,Ref:,Left:,Source:,SubDir: -n 'qpWrapper.sh' -- "$@"`
 eval set -- "$TEMP"
 
 function Helptext {
@@ -13,6 +13,7 @@ function Helptext {
 	echo -ne "-R, --Ref, --Right\tThe Right populations for your runs. Can be provided multiple times.\n"
 	echo -ne "-L, --Left, --Source\tThe Left Pops of your runs. Your Sample will be the first Left pop, followed by these. Can be provided multiple times.\n"
 	echo -ne "-D, --SubDir\tWhen provided, results will be placed in a subdirectory with the name provided within the result directory. Deeper paths can be provided by using '/'.\n"
+	echo -ne "-a, \t\tWhen provided, the option 'allsnps: YES' will NOT be provided.\n"
 }
 
 if [ $? -ne 0 ]
@@ -28,6 +29,7 @@ while true ; do
         -D|--SubDir) SUBDIR="$2"; shift 2;;
         --) TYPE=$2 ;shift 2; break ;;
         -h|--help) Helptext; exit 0 ;;
+		-a) ALLSNPS="FALSE"; shift 1;;
 	*) echo -e "invalid option provided.\n"; Helptext; exit 1;;
     esac
 done
@@ -35,9 +37,9 @@ done
 # EDIT ONLY THIS PART
 INDIR=/projects1/AncientFinnish/DataFreeze20_07_17/results/calls
 OUTDIR=/projects1/AncientFinnish/DataFreeze20_07_17/results #Subdirectories will be created within this directory to contain the results from the runs.
-GENO=$INDIR/Baltic.PublishedOnly.HO.1240K.Ancients+Saami.geno 
-SNP=$INDIR/Baltic.PublishedOnly.HO.1240K.Ancients+Saami.snp
-IND=$INDIR/Baltic.PublishedOnly.HO.1240K.Ancients+Saami.ind
+GENO=$INDIR/PublishedOnly.HO.1240K.Ancients+Saami.geno
+SNP=$INDIR/PublishedOnly.HO.1240K.Ancients+Saami.snp
+IND=$INDIR/PublishedOnly.HO.1240K.Ancients+Saami.indEach
 #DONT EDIT BELOW HERE
 
 OUTDIR2=$OUTDIR/$TYPE/$SUBDIR
@@ -69,6 +71,11 @@ for SAMPLE in "" ${SAMPLES[@]}; do
 	printf "popleft:\t$POPLEFT\n" >> $PARAMSFILE
 	printf "popright:\t$POPRIGHT\n" >>$PARAMSFILE
 	printf "details:\tYES\n" >>$PARAMSFILE
+	if [ $ALLSNPS="FALSE" ]; then
+	
+	else
+		printf "allsnps:\tYES\n" >>$PARAMSFILE
+	fi
 	
 	if [ "$SAMPLE" != "" ]; then
 		LOG=$OUTDIR2/Logs/$SAMPLE.$LEFTS.$RIGHTS.$TYPE.$(basename $TEMPDIR).log

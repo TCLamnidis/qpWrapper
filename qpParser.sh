eab73486a16fa09b222267e8b745dcf6d53c485f
TCLamnidis <thisseass@gmail.com>
2019-12-09 16:24:00 +0100

    Correct counting of outliers in later versions of qpGraph. Restructured code a bit

--- qpParser.sh
+++ qpParser.sh
@@ -33,7 +33,7 @@ while true ; do
     -c|--cutoff) cutoff="$2"; shift 2 ;;
     -d|--details) Details="TRUE"; shift;;
     -n|--newline) NewLine="FALSE"; shift;;
-    -s|--suffix) suffix=$2; set_suffix="TRUE"; shift 2 ;;
+    -s|--suffix) suffix="$2"; set_suffix="TRUE"; shift 2 ;;
     -h|--help) Helptext; exit 0 ;;
     *) break;;
 esac
@@ -51,126 +51,133 @@ if [[ ${set_suffix} != "TRUE" ]]; then
     suffix=".out"
 fi
 
-while read r; do
-    unset Lefts
-    unset Rights
-    unset temp3
-    unset temp4
-    switch1='off'
+if [[ $Type == "qpAdm" || $Type == "qpWave" ]]; then
+    while read r; do
+        unset Lefts
+        unset Rights
+        unset temp3
+        unset temp4
+        switch1='off'
     
     
     
-## qpAdm Parser
-    if [[ $Type == "qpAdm" ]]; then
-        switch2='off'
-        temp=$(grep -n "0  0" ${r} | tr -s " ")
-        temp1=$(echo $temp | sed -e 's/ /\t/g' | cut -f 6-)
-        let ResultLine=$(echo $temp | sed -e 's/ /\t/g' | cut -f 1 | rev | cut -c 2- | rev)+1
-        temp=$(grep -n "errors:" ${r} | tr -s " " | sed -e 's/ /\t/g' )
-        temp2=$(echo $temp | sed -e 's/ /\t/g' | cut -f 4-)
-        let ErrorLine=$(echo $temp | sed -e 's/ /\t/g' | cut -f 1 | rev | cut -c 2- | rev)+1
-        while read f; do
-            if [[ ${switch2} == 'on' && ${f} == "" ]]; then
-                break
-            elif [[ ${switch2} == 'on' ]]; then
-                Rights+=("$f")
-            elif [[ ${f} == "right pops:" ]]; then
-                switch2='on'
-            elif [[ ${switch1} == 'on' && ${f} != "" ]]; then
-                Lefts+=("$f")
-            elif [[ ${f} == "left pops:" ]]; then
-                switch1='on'
+    ## qpAdm Parser
+        if [[ $Type == "qpAdm" ]]; then
+            switch2='off'
+            temp=$(grep -n "0  0" ${r} | tr -s " ")
+            temp1=$(echo $temp | sed -e 's/ /\t/g' | cut -f 6-)
+            let ResultLine=$(echo $temp | sed -e 's/ /\t/g' | cut -f 1 | rev | cut -c 2- | rev)+1
+            temp=$(grep -n "errors:" ${r} | tr -s " " | sed -e 's/ /\t/g' )
+            temp2=$(echo $temp | sed -e 's/ /\t/g' | cut -f 4-)
+            let ErrorLine=$(echo $temp | sed -e 's/ /\t/g' | cut -f 1 | rev | cut -c 2- | rev)+1
+            while read f; do
+                if [[ ${switch2} == 'on' && ${f} == "" ]]; then
+                    break
+                elif [[ ${switch2} == 'on' ]]; then
+                    Rights+=("$f")
+                elif [[ ${f} == "right pops:" ]]; then
+                    switch2='on'
+                elif [[ ${switch1} == 'on' && ${f} != "" ]]; then
+                    Lefts+=("$f")
+                elif [[ ${f} == "left pops:" ]]; then
+                    switch1='on'
+                fi
+            done < <(cat ${r})
+            if [[ ${temp1} == *"..." ]] && [[ ${temp2} == *"..." ]]; then
+                temp1=${temp1%...}
+                temp2=${temp2%...}
+                temp3=$(sed "${ResultLine}q;d" ${r} | tr -s " " | sed -e 's/ /\t/g' | cut -f 2-)
+                temp4=$(sed "${ErrorLine}q;d" ${r} | tr -s " " | sed -e 's/ /\t/g' | cut -f 2-)
             fi
-        done < <(cat ${r})
-        if [[ ${temp1} == *"..." ]] && [[ ${temp2} == *"..." ]]; then
-            temp1=${temp1%...}
-            temp2=${temp2%...}
-            temp3=$(sed "${ResultLine}q;d" ${r} | tr -s " " | sed -e 's/ /\t/g' | cut -f 2-)
-            temp4=$(sed "${ErrorLine}q;d" ${r} | tr -s " " | sed -e 's/ /\t/g' | cut -f 2-)
-        fi
-        ProportionsPrint="${temp1}${temp3}"
-        ProportionsPrint="${ProportionsPrint%infeasible}"
-        sample=${Lefts[0]}
-        Sources=(${Lefts[@]:1})
-        joinSources=$(join_by , ${Sources[@]})
-        if [[ ${#Sources[@]} -lt 5 ]]; then
-            let Length=${#Sources[@]}+1
-            Buffer=$(repeatTab ${Length})
-        fi
-        if [[ $Details == "TRUE" ]]; then
-            OutGroup=$(join_by , ${Rights[@]})
-        fi
-        # echo -e "${ProportionsPrint%  }"
-        echo -e "${sample}\t${OutGroup}\t${joinSources}\t${ProportionsPrint%	}\t${Buffer}${temp2}${temp4}"
+            ProportionsPrint="${temp1}${temp3}"
+            ProportionsPrint="${ProportionsPrint%infeasible}"
+            sample=${Lefts[0]}
+            Sources=(${Lefts[@]:1})
+            joinSources=$(join_by , ${Sources[@]})
+            if [[ ${#Sources[@]} -lt 5 ]]; then
+                let Length=${#Sources[@]}+1
+                Buffer=$(repeatTab ${Length})
+            fi
+            if [[ $Details == "TRUE" ]]; then
+                OutGroup=$(join_by , ${Rights[@]})
+            fi
+            # echo -e "${ProportionsPrint%  }"
+            echo -e "${sample}\t${OutGroup}\t${joinSources}\t${ProportionsPrint%	}\t${Buffer}${temp2}${temp4}"
 
-## qpWave parsing
-    elif [[ $Type == "qpWave" ]]; then
-        switch2='off'
-        temp3=($(grep taildiff $r | tr -s " " | sed -e 's/ /\t/g' | rev | cut -f1 | rev))
-        while read f; do
-            if [[ ${switch2} == 'on' && ${f} == "" ]]; then
-                break
-            elif [[ ${switch2} == 'on' ]]; then
-                Rights+=("$f")
-            elif [[ ${f} == "right pops:" ]]; then
-                switch2='on'
-            elif [[ ${switch1} == 'on' && ${f} != "" ]]; then
-                Lefts+=("$f")
-            elif [[ ${f} == "left pops:" ]]; then
-                switch1='on'
+    ## qpWave parsing
+        elif [[ $Type == "qpWave" ]]; then
+            switch2='off'
+            temp3=($(grep taildiff $r | tr -s " " | sed -e 's/ /\t/g' | rev | cut -f1 | rev))
+            while read f; do
+                if [[ ${switch2} == 'on' && ${f} == "" ]]; then
+                    break
+                elif [[ ${switch2} == 'on' ]]; then
+                    Rights+=("$f")
+                elif [[ ${f} == "right pops:" ]]; then
+                    switch2='on'
+                elif [[ ${switch1} == 'on' && ${f} != "" ]]; then
+                    Lefts+=("$f")
+                elif [[ ${f} == "left pops:" ]]; then
+                    switch1='on'
+                fi
+            done < <(cat ${r})
+            joinLefts=$(join_by , ${Lefts[@]})
+            joinRights=$(join_by , ${Rights[@]})
+            if [[ $Details == "TRUE" ]]; then
+                for i in `seq 0 1 $(expr ${#temp3[@]} - 1)`; do 
+                    echo -e "${OutGroup}\t${joinRights}\t${joinLefts}\t${temp3[-i-1]}\twaves: $(expr ${#temp3[@]} - `expr ${i}`)"
+                done
+                if [[ $NewLine != "FALSE" ]]; then
+                    echo ""
+                fi
+            else
+                echo -e "${OutGroup}\t${joinRights}\t${joinLefts}\t${temp3[-1]}"
             fi
-        done < <(cat ${r})
-        joinLefts=$(join_by , ${Lefts[@]})
-        joinRights=$(join_by , ${Rights[@]})
-        if [[ $Details == "TRUE" ]]; then
-            for i in `seq 0 1 $(expr ${#temp3[@]} - 1)`; do 
-                echo -e "${OutGroup}\t${joinRights}\t${joinLefts}\t${temp3[-i-1]}\twaves: $(expr ${#temp3[@]} - `expr ${i}`)"
-            done
-            if [[ $NewLine != "FALSE" ]]; then
-                echo ""
-            fi
-        else
-            echo -e "${OutGroup}\t${joinRights}\t${joinLefts}\t${temp3[-1]}"
         fi
-    fi
-done < <(ls -1 ${fn0}/*${suffix})
+    done < <(ls -1 ${fn0}/*${suffix})
+fi
 
 if [[ ${set_suffix} != "TRUE" ]]; then
     suffix=".log"
 fi
 
 ## qpGraph logfile parsing
-while read r; do
-    let outlier_number=0
-    graph_switch1="off"
-    if [[ $Type == "qpGraph" ]]; then
-        while read f; do
-            if [[ ${f} == "## qpGraph version:"* ]]; then
-                let version=$(echo "${f}" | cut -d " " -f4)
-            elif [[ "${version}" -ge "5052" && ${f} == "score:"* ]]; then 
-                score=$(echo ${f} | cut -d " " -f 2)
-                # echo $score
-            elif [[ "${version}" -lt "5052" ]]; then 
-                score="NA"
-            elif [[ ${f} == "outliers:" ]]; then
-                graph_switch1="on"
-            elif [[ ${f} == "" ]]; then
-                graph_switch1="off"
-            elif [[ ${graph_switch1} == "on" ]]; then
-                let outlier_number+=1
-            elif [[ ${f} == "worst"* ]]; then
-                worst_stat=$(echo "${f}" | tr -s " " | cut -d " " -f 11)
-                # echo ${worst_stat}
+if [[ $Type == "qpGraph" ]]; then
+    while read r; do
+        let outlier_number=0
+        unset score
+        unset worst_stat
+        graph_switch1="off"
+            while read f; do
+                if [[ ${f} == "## qpGraph version:"* ]]; then
+                    let version=$(echo "${f}" | cut -d " " -f4)
+                elif [[ "${version}" -ge "5052" && ${f} == "score:"* ]]; then
+                    score=$(echo ${f} | cut -d " " -f 2)
+                    # echo $score
+                elif [[ "${version}" -ge "5052" && ${f} == "final score:"* ]]; then
+                    score=$(echo ${f} | cut -d " " -f 3)
+                    # echo $score
+                elif [[ "${version}" -lt "5052" ]]; then
+                    score="NA"
+                elif [[ ${f} == "outliers:" ]]; then
+                    graph_switch1="on"
+                elif [[ ${f} == "" ]]; then
+                    graph_switch1="off"
+                elif [[ ${graph_switch1} == "on" && ${f} != "Fit          Obs"* ]]; then
+                    let outlier_number+=1
+                elif [[ ${f} == "worst"* ]]; then
+                    worst_stat=$(echo "${f}" | tr -s " " | cut -d " " -f 11)
+                    # echo ${worst_stat}
+                fi
+            done < <(cat ${r})
+            if [[ -v cutoff ]] && (( $(echo "${worst_stat#-} > ${cutoff}"| bc -l) )); then
+                :
+            else
+                echo -e "$(basename ${r})\t${score}\t${worst_stat}\t${outlier_number}"
             fi
-        done < <(cat ${r})
-        if [[ -v cutoff ]] && (( $(echo "${worst_stat#-} > ${cutoff}"| bc -l) )); then
-            :
-        else
-            echo -e "$(basename ${r})\t${score}\t${worst_stat}\t${outlier_number}"
-        fi
-        # elif ![[ -v cutoff ]]; then
-#             echo -e "$(basename ${r})\t${score}\t${worst_stat}\t${outlier_number}"
-#         fi
-## echo ${outlier_number}
-    fi
-done < <(ls -1 ${fn0}/*${suffix})
+            # elif ![[ -v cutoff ]]; then
+    #             echo -e "$(basename ${r})\t${score}\t${worst_stat}\t${outlier_number}"
+    #         fi
+    ## echo ${outlier_number}
+    done < <(ls -1 ${fn0}/*${suffix})
+fi
